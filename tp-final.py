4728dea4c9e7f5081562acac2572c2d8bf066da4
absss03 <105992824+absss03@users.noreply.github.com>
2022-06-09 21:16:11 -0300

    Create tp-final.py

---
+++ tp-final.py
@@ -0,0 +1,216 @@
+
+
+import random
+
+# clave=1234
+# dni = 12345678
+clave=1
+clave_ingresada=0
+dni = 1
+dni_ingresado = 0
+reintentos=0
+opcion_menu_principal=0
+mensaje_menu_principal = "\n--------------- \n1. Consultas \n2. Retiros \n3. Transferencias \n4. Salir \n--------------- \nelija opcion >> "
+mensaje_menu_opcion_1 = "\n--------------- \nA - Posicion Global \nB - Movimientos \n--------------- \neliga opcion >> "
+mensaje_menu_opcion_3 = "\n--------------- \nA - Indique numero de cuenta destino \nB - volver  \n--------------- \nelija opcion >> "
+gastos=['INGRESO - DEPOSITO','EGRESO - SUPERMERCADO','EGRESO - CARNICERIA','EGRESO - EXTRACCION','EGRESO - RESTAURANTE','EGRESO - ZAPATERIA','EGRESO - TRANSFERENCIA','EGRESO - MASCOTERIA','INGRESO - TRANSFERENCIA','EGRESO - FARMACIA']
+tipo_de_moneda= "-------------\nA.Pesos\nB.Soles\n------------ \nSeleccione el tipo de moneda>> "
+moneda_elegida= 0
+saldo_pesos = 85000
+saldo_soles = 3564
+saldo= 0
+cuenta = 98765
+cuenta_ingresada = 0
+regresiva = 10
+devolucion= 0
+
+
+# ------------------------------
+def seleccionar_moneda():
+  moneda=' '
+  eleccion=True
+
+  while eleccion:
+    opcion=input(tipo_de_moneda)
+    if opcion=='A':
+      moneda='Pesos'
+      eleccion=False
+    if opcion=='B':
+      moneda='Soles'
+      eleccion=False
+
+  return moneda
+# ------------------------------
+def confirma(texto):
+  texto_impresion= f"Desea {texto} Si o No (S/N):"
+  opcion=' '
+  eleccion=True
+
+  while eleccion:
+    opcion=input(texto_impresion)
+    if opcion=='S':
+      eleccion=False
+    if opcion=='N':
+      eleccion=False
+  
+  return opcion
+# ------------------------------
+def menu_opcion_1(opcion_menu_principal):
+  while opcion_menu_principal == 1:
+    opcion_ingresada=input(mensaje_menu_opcion_1)
+
+    if opcion_ingresada == "A": # Posicion Global o Saldo
+      moneda_elegida = seleccionar_moneda()
+      if moneda_elegida=='Pesos':
+        if confirma('imprimir') == "N":
+          print(f"Su saldo disponible en {moneda_elegida} es de: ${saldo_pesos}")
+        else:
+          print("Su comprobante de Saldo se esta imprimiendo ...")
+      elif moneda_elegida=='Soles':
+        if confirma('imprimir') == "N":
+          print(f"Su saldo disponible en {moneda_elegida} es de: ${saldo_soles}")
+        else:
+          print("Su comprobante de Saldo se esta imprimiendo ...")
+
+      opcion_menu_principal=0
+    elif opcion_ingresada == "B": # Movimientos (al azar)
+      moneda_elegida = seleccionar_moneda()
+      if confirma('imprimir') == "N":                 #Si no quiere imprimir puede ver los movimientos por pantalla
+        print(f"Sus moviminetos en {moneda_elegida} son:")
+        #if devolucion > 0:
+        #  print(f"---> INGRESO $ {devolucion} por transferencia fallida") 
+        for i in range(10):
+          print(f"--->  {gastos[random.randint(0,9)]} : ${round(random.uniform(0.00, 999999.99),2)}")
+      else:         #Si pide imprimir no vera los movimientos... se "simula" que se impime un tiket
+        print("Su comprobante de Saldo se esta imprimiendo ...")
+      
+      opcion_menu_principal=0
+    else:
+      print("opcion no valida")
+# ------------------------------
+def menu_opcion_2(opcion_menu_principal, saldo_pesos, saldo_soles, clave_ingresada):
+  clave_ingresada=0
+  saldo=0
+  while opcion_menu_principal == 2:
+    moneda_elegida = seleccionar_moneda()    #Pide el ingreso de la moneda
+    if moneda_elegida=='Pesos':
+      saldo=saldo_pesos
+    else:
+      saldo=saldo_soles
+
+    #--------------------------------------------------------
+    monto_a_retirar = int(input("Ingrese monto a retirar: "))     #Pide el monto a retirar
+    if monto_a_retirar < saldo:
+      while clave_ingresada != clave:
+        clave_ingresada= int(input("Ingrese su clave: "))
+        if clave_ingresada == clave: 
+          saldo-=monto_a_retirar
+          if confirma('imprimir') == "N":
+            print("Transaccion realizada con exito")
+            opcion_menu_principal=0
+          else:
+            print("Su comprobante de Saldo se esta imprimiendo ...")
+            opcion_menu_principal=0
+    #--------------------------------------------------------
+    else:
+      print(f"el monto {monto_a_retirar} a retirar en {moneda_elegida} exede el saldo: {saldo}")
+      if confirma('salir') == "S":        #El monto exede el saldo, ¿quiere salir?
+        opcion_menu_principal=0
+      else:
+        #--------------------------------------------------------
+        monto_a_retirar = int(input("Ingrese monto a retirar 2: "))       #Si se queda puede modificar el monto 1 sola vez 
+        if monto_a_retirar < saldo:
+          while clave_ingresada != clave:
+            clave_ingresada= int(input("ingrese su clave: "))
+            if clave_ingresada == clave: 
+              saldo-=monto_a_retirar
+              if confirma('imprimir') == "N":
+                print("Transaccion realizada con exito:")
+                opcion_menu_principal=0
+              else:
+                print("Su comprobante de Saldo se esta imprimiendo ...")
+                opcion_menu_principal=0
+        #--------------------------------------------------------
+        opcion_menu_principal=0
+          
+  return saldo
+# ------------------------------
+def menu_opcion_3(opcion_menu_principal, saldo_pesos, saldo_soles, cuenta_ingresada, devolucion):
+  while opcion_menu_principal == 3:
+    cuenta_ingresada = int (input ("Ingrese numero de cuenta: "))
+    if seleccionar_moneda()=='Pesos': 
+      saldo=saldo_pesos
+    else:
+      saldo=saldo_soles
+      
+    monto_transferencia = int(input("Ingrese monto a transferir: "))
+    if monto_transferencia < saldo:
+      saldo-= monto_transferencia
+      if cuenta_ingresada != cuenta:
+        print("La cuenta ingresada no existe")    
+        #time.sleep(5)
+        #devolucion = monto_transferencia
+        saldo+= monto_transferencia
+        print(f"el monto {monto_transferencia} se devolvió su saldo es de {saldo}")
+      if cuenta_ingresada == cuenta:
+        print("Transferencia realizada con exito")
+    else:
+      print("Saldo insuficiente")
+    opcion_menu_principal = 0
+
+  return saldo  #, devolucion
+
+
+
+while reintentos<3 and opcion_menu_principal!=4 and dni_ingresado==0:
+  clave_ingresada= int(input("ingrese su clave: "))   
+  if clave_ingresada != clave:     #si la clave no es correcta
+    print(f"La clave ingresada {clave_ingresada}, no es valida")
+    reintentos+=1 
+  else:    #cuando la clave es correcta
+    dni_ingresado = int(input(" ingrese su dni: "))
+    while dni_ingresado != dni:     #si lel dni no es correcto
+      print(f"El nro de DNI ingresado {dni_ingresado}, no es valido")
+      dni_ingresado = int(input(" ingrese su dni: "))
+  if clave_ingresada==clave and dni_ingresado==dni:
+    # ------------------------------
+    # Menu Principal
+    #
+    while opcion_menu_principal!=4:
+      opcion_menu_principal = int(input(mensaje_menu_principal))
+      # ------------------------------
+      # Ejecuto el Menu de la Opcion 1
+      menu_opcion_1(opcion_menu_principal)
+
+      # ------------------------------
+      # Menu Opcion 2
+      #
+      saldo=menu_opcion_2(opcion_menu_principal, saldo_pesos, saldo_soles, clave_ingresada)
+      if moneda_elegida=='Pesos':
+        saldo_pesos=saldo
+      else:
+        saldo_soles=saldo
+
+      # ------------------------------              
+      # Menu Opcion 3
+      #
+      saldo=menu_opcion_3(opcion_menu_principal, saldo_pesos, saldo_soles, cuenta_ingresada, devolucion)
+      if moneda_elegida=='Pesos':
+        saldo_pesos=saldo
+        #devolucion_pesos= devolucion
+      else:
+        saldo_soles=saldo 
+        #devolucion_soles= devolucion
+
+    # ------------------------------              
+    # Cuenta regresiva
+    # no es necesario pero me parecio piola
+    #import time 
+    #while regresiva > 0: 
+    #  print (f'Retire su tarjeta en {regresiva}') 
+    #  regresiva-=1
+    #  time.sleep(1)
+    #print("ups! el cajero se comio tu tarjeta \nla proxima hay que ser mas rapidos :)")
+
+
+    
