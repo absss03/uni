c5dd4918d69a21f61d2b5a1a5eba822c63939d9e
absss03 <barbieriabigailok@gmail.com>
2022-06-20 14:50:33 -0300

    creado

---
+++ func.py
@@ -0,0 +1,34 @@
+
+# def compara(numero, otro_numero):
+  
+#   if numero - otro_numero == 0:
+#     status= 0
+#   if numero - otro_numero > 0:
+#     status = 1
+#   else:
+#     status = -1
+
+#   return status
+
+# status = compara(numero, otro_numero)
+# print({status})
+
+numero=3
+otro_numero= 6
+
+# def suma_lenta(numero, otro_numero):
+#   contador = 0
+#   suma=numero
+#   if otro_numero > 0:
+#     while contador < otro_numero:
+#       suma+= 1
+#       contador += 1 
+#   else:
+#     while contador > otro_numero:
+#       suma-= 1
+#       contador -= 1
+  
+#   return suma
+
+
+def division_lenta(dividendo, divisor):
